a174e3e72efd76b778bcd7d8fe27da7848edd81a
Filip Troníček <filip@gitpod.io>
2024-12-08 19:38:14 +0100

    Format file

--- flake.nix
+++ flake.nix
@@ -9,193 +9,205 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager, ... }:
-  let
-    vars = {
-      defaultbrowser = "firefoxdeveloperedition";
-    };
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-    [
-      pkgs.neovim
-        pkgs.gh
-        pkgs.git
-        pkgs.wget
-        pkgs.yt-dlp
-        pkgs.rclone
-        pkgs.lolcat
-        pkgs.awscli2
-        pkgs.bun
-        pkgs.pre-commit
-        pkgs.pandoc
-        pkgs.imagemagick
-        pkgs.fastfetch
-        pkgs.coreutils
-        pkgs.ffmpeg
-        pkgs.zoxide # todo: do the whole dance with zoxide
-        pkgs.jq
-        pkgs.yq
-        pkgs.gnupg
-        pkgs.unixtools.watch
-        pkgs.nixd
-        pkgs.python314
-        pkgs.defaultbrowser  # Added defaultbrowser package
-
-        # GUIs
-        pkgs.git-credential-manager
-        pkgs.audacity
-        pkgs.raycast
-        pkgs.qbittorrent
-        pkgs.obsidian
-        pkgs.warp-terminal
-        pkgs.monitorcontrol
-        pkgs.wireshark-qt
-
-        pkgs.knot-dns
-      ];
-
-      # Add activation script for defaultbrowser
-      system.activationScripts.postUserActivation.text = ''
-        defaultbrowser ${vars.defaultbrowser}
-      '';
-
-      nixpkgs.config.allowUnfree = true;
-
-      # Rest of your configuration remains the same...
-      fonts.packages = [
-        pkgs.lexend
-      ];
-
-      users.users.filip = {
-        name = "filip";
-        home = "/Users/filip";
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      home-manager,
+      ...
+    }:
+    let
+      vars = {
+        defaultbrowser = "firefoxdeveloperedition";
       };
-
-      homebrew = {
-        enable = true;
-        brews = [
-          "bettercap"
-          "gnupg"
-          "go"
-          "nvm"
-          "handbrake"
-        ];
-        casks = [
-          "figma"
-          "loom"
-          "ukelele"
-          "amie"
-          "orbstack"
-          "linear-linear"
-          "nvidia-geforce-now"
-          "shottr"
-          "tailscale"
-          "cursor"
-          "stats"
-          "karabiner-elements"
-          "vlc"
-          "rustdesk"
-          "handbrake"
-          "1password"
-          "blender"
-          "thunderbird"
-          "lunar-client"
-          "steam"
-          "zotero"
-          "github"
-          "jetbrains-toolbox"
-          "adobe-creative-cloud"
-          "zed"
-          "visual-studio-code"
-          "vscodium"
-          "utm"
-          "signal"
-          "microsoft-powerpoint"
-          "discord"
-          "slack"
-          "parsec"
-          "obs"
-          "meetingbar"
-
-          "tor-browser"
-          "arc"
-          "firefox"
-          "firefox@nightly"
-          "firefox@developer-edition"
-          "eloston-chromium"
-        ];
-
-        onActivation.cleanup = "zap";
-      };
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.persistent-apps = [
-          "/System/Volumes/Data/Applications/Firefox Developer Edition.app"
-          "/System/Volumes/Data/Applications/Thunderbird.app"
-          "/System/Volumes/Data/Applications/Slack.app"
-          "/System/Volumes/Data/Applications/Discord.app"
-        ];
-
-        finder.AppleShowAllExtensions = true;
-        finder.AppleShowAllFiles = true;
-        loginwindow.GuestEnabled = false;
-        NSGlobalDomain."com.apple.swipescrolldirection" = false;
-        NSGlobalDomain.AppleICUForce24HourTime = true;
-        NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
-      };
-
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 5;
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#mbp
-    darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-        nix-homebrew.darwinModules.nix-homebrew
-        home-manager.darwinModules.home-manager
+      configuration =
+        { pkgs, ... }:
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.filip = { pkgs, ... }: {
-            home.stateVersion = "24.05";
-            programs.git = {
-              enable = true;
-              userName = "Filip Troníček";
-              userEmail = "filip.tronicek@seznam.cz";
-              extraConfig = {
-                init.defaultBranch = "main";
-                pull.rebase = true;
-                core.editor = "code --wait";
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.neovim
+            pkgs.gh
+            pkgs.git
+            pkgs.wget
+            pkgs.yt-dlp
+            pkgs.rclone
+            pkgs.lolcat
+            pkgs.awscli2
+            pkgs.bun
+            pkgs.pre-commit
+            pkgs.pandoc
+            pkgs.imagemagick
+            pkgs.fastfetch
+            pkgs.coreutils
+            pkgs.ffmpeg
+            pkgs.zoxide # todo: do the whole dance with zoxide
+            pkgs.jq
+            pkgs.yq
+            pkgs.gnupg
+            pkgs.unixtools.watch
+            pkgs.nixd
+            pkgs.python314
+            pkgs.defaultbrowser
+            pkgs.nixfmt-rfc-style
 
-                commit.gpgSign = true;
-                gpg.program = "${pkgs.gnupg}/bin/gpg";
-              };
-            };
-            programs.zoxide = {
-              enable = true;
-              enableZshIntegration = true;
-            };
+            # GUIs
+            pkgs.git-credential-manager
+            pkgs.audacity
+            pkgs.raycast
+            pkgs.qbittorrent
+            pkgs.obsidian
+            pkgs.warp-terminal
+            pkgs.monitorcontrol
+            pkgs.wireshark-qt
+
+            pkgs.knot-dns
+          ];
+
+          # Add activation script for defaultbrowser
+          system.activationScripts.postUserActivation.text = ''
+            defaultbrowser ${vars.defaultbrowser}
+          '';
+
+          nixpkgs.config.allowUnfree = true;
+
+          # Rest of your configuration remains the same...
+          fonts.packages = [
+            pkgs.lexend
+          ];
+
+          users.users.filip = {
+            name = "filip";
+            home = "/Users/filip";
           };
 
-          nix-homebrew = {
+          homebrew = {
             enable = true;
-            enableRosetta = true;
-            user = "filip";
-            autoMigrate = true;
-          };
-        }
-      ];
-    };
+            brews = [
+              "bettercap"
+              "gnupg"
+              "go"
+              "nvm"
+              "handbrake"
+            ];
+            casks = [
+              "figma"
+              "loom"
+              "ukelele"
+              "amie"
+              "orbstack"
+              "linear-linear"
+              "nvidia-geforce-now"
+              "shottr"
+              "tailscale"
+              "cursor"
+              "stats"
+              "karabiner-elements"
+              "vlc"
+              "rustdesk"
+              "handbrake"
+              "1password"
+              "blender"
+              "thunderbird"
+              "lunar-client"
+              "steam"
+              "zotero"
+              "github"
+              "jetbrains-toolbox"
+              "adobe-creative-cloud"
+              "zed"
+              "visual-studio-code"
+              "vscodium"
+              "utm"
+              "signal"
+              "microsoft-powerpoint"
+              "discord"
+              "slack"
+              "parsec"
+              "obs"
+              "meetingbar"
 
-    darwinPackages = self.darwinConfigurations."mbp".pkgs;
-  };
+              "tor-browser"
+              "arc"
+              "firefox"
+              "firefox@nightly"
+              "firefox@developer-edition"
+              "eloston-chromium"
+            ];
+
+            onActivation.cleanup = "zap";
+          };
+
+          system.defaults = {
+            dock.autohide = true;
+            dock.persistent-apps = [
+              "/System/Volumes/Data/Applications/Firefox Developer Edition.app"
+              "/System/Volumes/Data/Applications/Thunderbird.app"
+              "/System/Volumes/Data/Applications/Slack.app"
+              "/System/Volumes/Data/Applications/Discord.app"
+            ];
+
+            finder.AppleShowAllExtensions = true;
+            finder.AppleShowAllFiles = true;
+            loginwindow.GuestEnabled = false;
+            NSGlobalDomain."com.apple.swipescrolldirection" = false;
+            NSGlobalDomain.AppleICUForce24HourTime = true;
+            NSGlobalDomain.AppleInterfaceStyleSwitchesAutomatically = true;
+          };
+
+          services.nix-daemon.enable = true;
+          nix.settings.experimental-features = "nix-command flakes";
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+          system.stateVersion = 5;
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#mbp
+      darwinConfigurations."mbp" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.filip =
+              { pkgs, ... }:
+              {
+                home.stateVersion = "24.05";
+                programs.git = {
+                  enable = true;
+                  userName = "Filip Troníček";
+                  userEmail = "filip.tronicek@seznam.cz";
+                  extraConfig = {
+                    init.defaultBranch = "main";
+                    pull.rebase = true;
+                    core.editor = "code --wait";
+
+                    commit.gpgSign = true;
+                    gpg.program = "${pkgs.gnupg}/bin/gpg";
+                  };
+                };
+                programs.zoxide = {
+                  enable = true;
+                  enableZshIntegration = true;
+                };
+              };
+
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "filip";
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      darwinPackages = self.darwinConfigurations."mbp".pkgs;
+    };
 }
