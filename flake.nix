7a6154bfb8cd2ee564331ed2a8db53a5c016e1d7
Filip Troníček <filip@gitpod.io>
2025-03-02 11:06:18 +0100

    Fix `/etc/pam.d/sudo_local`

--- flake.nix
+++ flake.nix
@@ -103,6 +103,12 @@
             end tell'
           '';
 
+          environment.etc."pam.d/sudo_local".text = ''
+            # sudo_local: local config file which survives system update and is included for sudo
+            # uncomment following line to enable Touch ID for sudo
+            auth       sufficient     pam_tid.so
+          '';
+
           nixpkgs.config.allowUnfree = true;
 
           # Rest of your configuration remains the same...
